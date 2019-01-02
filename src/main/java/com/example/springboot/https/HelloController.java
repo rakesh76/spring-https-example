328cb2ee397881511f2dcf999b29f4200ab496ed
Rakesh <rakesh.sanghvi@gmail.com>
2019-01-01 21:26:58 -0600

    spring boot https example

---
+++ src/main/java/com/example/springboot/https/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.springboot.https;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "This is Secured Rest Service ";
+    }
+}
---
+++ src/main/java/com/example/springboot/https/SpringBootHttpsApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboot.https;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHttpsApplication.class, args);
+	}
+
+}
+
